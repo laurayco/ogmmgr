62b0b7d90221f73458ec32f7558732a55ef86e1f
Lauren <laurayco@outlook.com>
2019-06-05 18:46:51 -0500

    added some compiler options

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { ModeEntry, ModeIndex, AuthorInfo } from "./models";
+
+/*
+    this file is the entry point to build the website.
+*/
+
+async function get_game_mode_entries()
+: Promise<ModeEntry[]> {
+    return [];
+}
