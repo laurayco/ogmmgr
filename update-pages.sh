d83c746296090163101f5f0d4bd5365ef64dd1ae
Lauren <laurayco@outlook.com>
2019-06-06 02:14:15 -0500

    up to date shell script

--- update-pages.sh
+++ update-pages.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 
+# step one: build
+npm run build
+
+# step two: run
+npm run start
+
+# step three: switch to gh-pages branch.
+git checkout gh-pages
+
+# step four: pull to avoid conflicts.
+git pull
+
+# step five: move everything from www to root folder.
+pushd www
+mv *.* ..
+popd www
