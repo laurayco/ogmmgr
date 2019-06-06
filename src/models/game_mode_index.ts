69e27f3299fb551b35e094b72a6bc1d24cf5fe13
Lauren <laurayco@outlook.com>
2019-06-05 21:18:19 -0500

    created regex validator functions.

--- src/models/game_mode_index.ts
+++ src/models/game_mode_index.ts
@@ -5,12 +5,25 @@ import * as PropTypes from "prop-types";
     render on the website.
 */
 
+const regex_prop_validator = (regex: RegExp) => (props: any, propName: string, componentName: string)=>{
+    if(!regex.test(props[propName])) {
+        return new Error(
+            'Invalid prop `' + propName + '` supplied to' +
+            ' `' + componentName + '`. Validation failed.'
+        );
+    }
+};
+
 export type GAME_CODE = string;
+export const GAME_CODE_PROP = regex_prop_validator(/abc/);
 export type GITHUB_SHA = string;
-export type GAME_MODE_CODE = string;
+export const GITHUB_SHA_PROP = regex_prop_validator(/abc/);
 export type DATE_STRING = string;
+export const DATE_STRING_PROP = regex_prop_validator(/abc/);
 export type URL = string;
+export const URL_PROP = regex_prop_validator(/abc/);
 export type REPO_FILENAME = string;
+export const REPO_FILENAME_PROP = regex_prop_validator(/^(?!.{256,})(?!(aux|clock\$|con|nul|prn|com[1-9]|lpt[1-9])(?:$|\.))[^ ][ \.\w-$()+=[\];#@~,&amp;']+[^\. ]$/i);
 
 type PropTypesInterface<T> = {
     [key in keyof T]: PropTypes.Validator<any>;
@@ -58,7 +71,7 @@ export interface ModeIndex {
 export const ModeIndexProps :
 PropTypesInterface<ModeIndex> = {
     name: PropTypes.string,
-    author: PropTypes.oneOfType(null),
+    author: PropTypes.shape(AuthorInformationProps),
     description: PropTypes.string,
-    history: PropTypes.arrayOf(PropTypes.oneOfType(null))
+    history: PropTypes.arrayOf(PropTypes.shape(ModeEntryProps))
 };
