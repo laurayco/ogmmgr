8a63fce2b1157e2dbac65aa1e8cc01beba272a33
Lauren <laurayco@outlook.com>
2019-10-04 22:33:17 -0500

    add text mode alternative for read_file

--- src/data/utils.ts
+++ src/data/utils.ts
@@ -4,6 +4,13 @@ import { parse as parse_yaml } from "yaml";
 
 export const read_directory = promisify(readdir);
 export const read_file = promisify(readFile);
+export const read_text_file = (fn: string|number|Buffer|URL, opts?: {
+    encoding?: string,
+    flag?: string
+})=>read_file(fn, Object.assign({
+    encoding: "utf8"
+}, opts));
+
 export const file_meta = promisify(stat);
 export const read_yaml = async <T>(fname) => {
     const contents = await read_file(fname, {
