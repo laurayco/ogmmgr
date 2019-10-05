c69293fc82e4e6ae6149123295ed4b763d587c61
Lauren <laurayco@outlook.com>
2019-10-04 22:33:46 -0500

    add code field

--- src/models/mode.ts
+++ src/models/mode.ts
@@ -4,10 +4,12 @@ import { PropTypesInterface, DATE_STRING, REPO_FILENAME, URL_PROP } from "./util
 export interface ModeEntry {
     share_code: string; // what the game client gives for you to share.
     published: DATE_STRING; // when this entry was published.
+    code: string;
 };
 
 export const ModeEntryProps :
 PropTypesInterface<ModeEntry> = {
     share_code: PropTypes.string,
-    published: PropTypes.string
+    published: PropTypes.string,
+    code: PropTypes.string
 };
