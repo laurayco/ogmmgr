b529cbbc83d84bc73a9699d5b7dcf4c8be9d9407
Lauren <laurayco@outlook.com>
2019-10-07 18:57:13 -0500

    method to load a mode index from fs

--- src/data/mode.ts
+++ src/data/mode.ts
@@ -1,31 +1,31 @@
 import { read_directory, read_file, read_yaml, file_meta, read_text_file } from "./utils";
 import { join, parse } from "path";
 import CONFIG from "../config";
-import { ModeEntry } from "../models/mode";
 
-const load_mode_entries = async (author: string, slug: string) => {
+import { ModeIndex } from "../models/mode_index";
+import { ModeEntry } from "../models";
+
+const load_mode = async (author: string, slug: string) => {
     const data_directory = await CONFIG.data_directory;
-    const mode_dname = join(data_directory, "modes", author, slug);
-    const filenames = await read_directory(mode_dname, {withFileTypes: true});
-    const gmfiles = filenames.filter(fn=>fn.isFile&&fn.name.endsWith("owgm")).map(fn=>join(mode_dname, fn.name));
-    const results = await Promise.all(gmfiles.map(async fn=>({
-        fn,
-        stats: await file_meta(fn)
-    })));
-    results.sort((a,b)=>a.stats.mtime.getTime()-b.stats.mtime.getTime());
-    return Promise.all(results.map(async entry_file_info=>{
-        const { fn, stats } = entry_file_info;
-        const parsed_fn = parse(fn);
-        const share_code = parsed_fn.name;
-        const published = stats.mtime.toISOString();
-        const code = await read_text_file(fn);
-        return { share_code, code, published } as ModeEntry;
-    }));
+    const mode_dname = join(data_directory, author, `${slug}.yml`);
+    const mode_entry = await read_yaml(mode_dname);
+    const current_ts = (new Date()).toISOString();
+    const history = Object.keys(mode_entry["codes"]).map(share_code=>{
+        return {
+            code: mode_entry["codes"][share_code]["script"] || null,
+            published: mode_entry["codes"][share_code]["published"] || current_ts,
+            share_code
+        } as ModeEntry;
+    });
+    const mi : ModeIndex = {
+        author,
+        description: mode_entry["description"] || `#${slug}`,
+        history,
+        name: mode_entry["name"] || slug,
+        preview_image: mode_entry["image"] || null,
+        preview_video: mode_entry["video"] || null
+    }
+    return mi as ModeIndex;
 }
 
-const load_mode_description = async (author: string, slug: string) => {
-    const data_directory = await CONFIG.data_directory;
-    const mode_dname = join(data_directory, "modes", author, slug);
-    const mode_desc_fn = join(mode_dname, "description.md");
-    
-};
+export default load_mode;
