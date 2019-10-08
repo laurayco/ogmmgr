173eda4683e4bdf6bbd8088606511efc6c5c7cb5
Lauren <laurayco@outlook.com>
2019-10-07 20:23:26 -0500

    delete consts.json file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import * as express from "express";
-import * as utils from "./utils";
-import * as CONSTS from "./consts.json";
+import { resolve } from "path";
+import config from "./config";
 
-const app = express();
+async function main() {
+    const port = await config.port;
+    const htmldir = await config.output_directory;
+    const app = express();
+    
+    app.use(express.static(resolve(htmldir)));
+    
+    app.listen(port);
+}
 
-app.use(express.static(utils.actual_filename("./www")));
-
-app.listen(CONSTS.DEV_PORT);
+main();
