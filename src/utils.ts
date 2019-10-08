dc7f07f01b20f72e31392f6c9b0a709348c4ba4f
Lauren <laurayco@outlook.com>
2019-10-07 19:26:48 -0500

    remove unused code

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,3 @@
-// nodejs imports
-import * as fs from "fs";
-import * as path from "path";
-import { promisify } from "util";
-
-export const readFile = promisify(fs.readFile);
-export const writeFile = promisify(fs.writeFile);
-export const fileStat = promisify(fs.stat);
-export const readdir = promisify(fs.readdir);
-export const fileExists = promisify(fs.exists);
-export const isFile = promisify(fs.readdir)
-
-// npm package imports
 import * as checkPropTypes from 'check-prop-types';
 import { ValidationMap } from "prop-types";
 import * as marked_func from "marked";
@@ -23,9 +10,6 @@ export const marked = async (src: string, opts?: marked_func.MarkedOptions) : Pr
         });
     });
 };
-
-// project local imports
-import * as CONSTS from "./consts.json";
 
 export const test_types = <T=any>(
     tmaps: ValidationMap<T>,
@@ -40,27 +24,6 @@ export const test_types = <T=any>(
         throw new Error(comparison_message);
     }
 };
-
-export const actual_filename = repo_filename=>path.resolve(repo_filename);
-
-export const only_directories = async (root: string,filenames: string[])=>{
-    const filestats = await Promise.all(filenames.map(async fn=>{
-        const stats = await fileStat(path.resolve(root,fn));
-        return {stats,fname: fn};
-    }));
-    return filestats.filter(fs=>fs.stats.isDirectory()).map(fs=>fs.fname);
-};
-
-export const only_files = async (root: string,filenames: string[])=>{
-    const filestats = await Promise.all(filenames.map(async fn=>{
-        const stats = await fileStat(path.resolve(root,fn));
-        return {stats,fname: fn};
-    }));
-    return filestats.filter(fs=>fs.stats.isFile()).map(fs=>fs.fname);
-};
-
-export const sub_directories = async root=>only_directories(root, await readdir(root));
-export const directory_files = async root=>only_files(root, await readdir(root));
 
 export function flatten(arr: any[]) {
     return arr.reduce(function (flat, toFlatten) {
