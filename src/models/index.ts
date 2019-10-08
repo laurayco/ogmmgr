1d027eb7b1017ce366a2929a81e33c66709404cf
Lauren <laurayco@outlook.com>
2019-10-07 19:24:35 -0500

    add author index to exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,13 +1,16 @@
 import { AuthorInformation, AuthorInformationProps } from "./author";
+import { AuthorIndex, AuthorIndexProps } from "./author_index";
 import { ModeEntry, ModeEntryProps } from "./mode";
 import { ModeIndex, ModeIndexProps } from "./mode_index";
 
 export type AuthorInfo = AuthorInformation;
 export type ModeEntry = ModeEntry;
 export type ModeIndex = ModeIndex;
+export type AuthorIndex = AuthorIndex;
 
 export default {
     AuthorInfo: AuthorInformationProps,
     ModeEntry: ModeEntryProps,
-    ModeIndex: ModeIndexProps
+    ModeIndex: ModeIndexProps,
+    AuthorIndex: AuthorIndexProps
 };
