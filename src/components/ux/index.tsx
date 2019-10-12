8d843cc55a13e478219ee6916597744844fa6224
Lauren <laurayco@outlook.com>
2019-10-11 19:03:22 -0500

    trying to get page layout down

--- src/components/ux/index.tsx
+++ src/components/ux/index.tsx
@@ -1,26 +1,41 @@
 import * as React from "react";
 import { Route } from "react-router-dom";
-import { CssBaseline } from "@material-ui/core"
+import { CssBaseline, Grid } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles";
 
 interface Props {
 
 };
 
+const useStyles = makeStyles(theme=>({
+    expand: {
+        flexGrow: 1
+    }
+}));
+
 export default (props: Props)=>{
-    return <>
+
+    const styles = useStyles();
+
+    return <Grid container direction="column" spacing={0}>
         <CssBaseline />
-        <div>
-            <strong>HEADER</strong>
-        </div>
-        <p>BODY</p>
-        <Route path="/p/:page">
-            <p>PAGE</p>
-        </Route>
-        <Route exact path="/a/:author">
-            <p>AUTHOR</p>
-        </Route>
-        <Route path="/a/:author/m/:mode">
-            <p>MODE</p>
-        </Route>
-    </>;
+        <Grid item className="header">
+            HEADER.
+        </Grid>
+        <Grid item xs className={`body `}>
+            BODY.
+            <Route path="/p/:page">
+                <p>PAGE</p>
+            </Route>
+            <Route exact path="/a/:author">
+                <p>AUTHOR</p>
+            </Route>
+            <Route path="/a/:author/m/:mode">
+                <p>MODE</p>
+            </Route>
+        </Grid>
+        <Grid item className="footer">
+            FOOTER.
+        </Grid>
+    </Grid>;
 };
