543d1c2012a0ed5c532f5915ea235b1e344bf90e
Lauren Connors <laurayco@outlook.com>
2019-10-11 22:37:37 -0500

    Update client.tsx

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App, { DataBank } from "./components";
-
 import "./style.css";
 
 export default async (databank: DataBank)=>{
     const root_el = document.querySelector("#root");
+    const base_url = process.env.base_url;
     return new Promise((resolve,reject)=>{
-        ReactDOM.hydrate(<App databank={databank} />, root_el, ()=>{
+        ReactDOM.hydrate(<App {... {base_url, databank}}/>, root_el, ()=>{
             resolve();
         });
     });
