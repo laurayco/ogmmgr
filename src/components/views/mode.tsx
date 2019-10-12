b9becd17af7b309b2e968915286b93f752a245fc
Lauren <laurayco@outlook.com>
2019-10-11 21:19:12 -0500

    get mode data effect / rendering

--- src/components/views/mode.tsx
+++ src/components/views/mode.tsx
@@ -1,14 +1,34 @@
 import * as React from "react";
-import { Route } from "react-router-dom";
+import { useRouteMatch } from "react-router-dom";
+import useMode from "../../effects/data/mode";
 
 interface Props {
 
 };
 
 const ModeView = (props: Props)=>{
-    return <Route path="/a/:author/m/:mode">
-        Viewing a mode!
-    </Route>;
+    const match = useRouteMatch<{
+        author: string,
+        mode: string
+    }>({
+        path: `/a/:author/m/:mode`,
+        exact: true
+    });
+
+    const author_ref = match ? match.params.author : null;
+    const mode_ref = match ? match.params.mode : null;
+
+    const mode_index = useMode(author_ref, mode_ref);
+
+    if(match) {
+        if(mode_index) {
+            return <div>{JSON.stringify(mode_index,(key,val)=>val,"  ")}</div>;
+        } else {
+            return <div>loading...</div>
+        }
+    } else {
+        return null;
+    }
 };
 
 export default ModeView;
